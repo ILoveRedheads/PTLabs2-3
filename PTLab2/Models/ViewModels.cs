28e7a31ea032c68c998bb438f8e54f5c6c88e0d6
Иван Плотников <heybuddynicecock@gmail.com>
2023-11-14 12:25:58 +0300

    Выгружены 2 и 3 лабы

---
+++ PTLab2/Models/ViewModels.cs
@@ -0,0 +1,30 @@
+﻿namespace PTLab2.Models
+{
+    // Модель пользователя
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Mail { get; set; }
+        public string? Password { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime Birthday { get; set; }
+    }
+    // Модель товара
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public float Price { get; set; }
+        public int Count { get; set; }
+    }
+    // Модель корзины
+    public class Basket
+    {
+        public int Id { get; set; }
+        public string? ProductName { get; set; }
+        public float ProductPrice { get; set; }
+        public int ProductCount { get; set; }
+        public float Amount { get; set; }
+    }
+}
---
+++ PTLab2Test/AddDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PTLab2.Models;
+
+
+namespace PTLab2Test
+{
+    public class AddDbContext
+    {
+        public static ServiceCollection InitilizeServices()
+        {
+            var services = new ServiceCollection();
+            // используем бд из памати
+            var options = new DbContextOptionsBuilder<ShopContext>().UseInMemoryDatabase("Shop.db").Options;
+            // добавляем контекст ShopContext в качестве сервиса в приложение
+            services.AddScoped(newOptions => new ShopContext(options));
+            return services;
+        }
+
+    }
+}
